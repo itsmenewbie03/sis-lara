bcd67886900ec3e0efce06e5b6a6ffa27cd711ac
Toshiven <kevin.abler19@gmail.com>
2024-02-28 15:03:10 +0800

    feat(subject): add read, create, delete, and update for subject page

---
+++ resources/views/subject/edit.blade.php
@@ -0,0 +1,24 @@
+@extends("dashboard")
+@section("content")
+Edit Subject
+@endsection
+@section("form")
+<form action="{{ route('subject.update', $subject->id) }}" method="POST" id="editUserForm">
+    @csrf
+    @method('PATCH')
+    <div class="modal-body">
+        <div class="form-group">
+            <label for="subjectName">Subject Name</label>
+            <input type="text" class="form-control" name="subjectName" id="subjectName" value="{{$subject->subjectName}}" required>
+        </div>
+        <div class="form-group">
+            <label for="subjectCode">Subject Code</label>
+            <input type="text" class="form-control" name="subjectCode" id="subjectCode" value="{{$subject->subjectCode}}" required>
+        </div>
+    </div>
+    <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Update</button>
+    </div>
+</form>
+@endsection
--- resources/views/subject/index.blade.php
+++ resources/views/subject/index.blade.php
@@ -2,3 +2,65 @@
 @section("content")
 Subject Dashboard
 @endsection
+@section("table")
+
+<div class="modal" tabindex="-1" role="dialog" id="addSubjectModal">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Add Subject</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                @include("components.add-subject-form");
+            </div>
+        </div>
+    </div>
+</div>
+
+<button class="btn btn-primary float-right" data-toggle="modal" data-target="#addSubjectModal">Add</button>
+<table id="myTable" class="display">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Subject Code</th>
+            <th>Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($subjects as $subject)
+        <tr>
+            <td>{{$subject->id}}</td>
+            <td>{{$subject->subjectName}}</td>
+            <td>{{$subject->subjectCode}}</td>
+            <td>
+                <form method="POST" action={{route("subject.destroy",$subject->id)}}>
+                    {{ csrf_field() }}
+                    {{ method_field('DELETE') }}
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-danger" data-toggle="tooltip" title="Delete Subject">
+                            <i class="fas fa-user-times"></i>
+                        </button>
+                    </div>
+                    <a href="{{route('subject.edit',$subject->id)}}" class="btn btn-primary" data-toggle="tooltip" title="Edit Subject">
+                        <i class="fas fa-user-edit"></i>
+                    </a>
+                </form>
+ 
+            </td>
+       </tr>
+        @endforeach
+    </tbody>
+</table>
+@endsection
+@push("scripts")
+<script>
+    $(document).ready(function() {
+        $('#myTable').DataTable();
+    });
+</script>
+@endpush
